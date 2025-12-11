d24ba20fd26820e3dd40d29704f9e5c2dcb2c750
zxz19940604-blip <zxz19940604@gmail.com>
2025-12-11 21:21:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >>feeds.conf.default
+echo 'src-git fancontrol https://github.com/LeeHe-gif/luci-app-fancontrol.git' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
